3b60f9c5876cf2413caa9fa3317b3c47a3b8656a
Axel Kalland <axelkalland@gmail.com>
2019-05-17 12:39:39 +0200

    more

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
@@ -7,11 +8,15 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
 
     if @cocktail.save
-      redirect_to cocktail(@cocktail)
+      redirect_to cocktails_path
     else
       render :new
     end
@@ -19,13 +24,7 @@ class CocktailsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :picture_url)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,5 @@
 class DosesController < ApplicationController
   def new
-    # we need @restaurant in our `simple_form_for`
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
@@ -8,7 +7,9 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+
     @dose.cocktail = @cocktail
+
 
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -17,9 +18,15 @@ class DosesController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    dose = Dose.find(params[:id])
+    dose.destroy
 
+    redirect_to cocktail_path(dose.cocktail)
+  end
+
+  private
   def dose_params
-    params.require(:dose).permit(:content, :rating)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
