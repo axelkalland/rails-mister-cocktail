7474e63309fae2dc0d54d28dc0d2fab1e2dfe79a
Axel Kalland <axelkalland@gmail.com>
2019-05-17 12:57:25 +0200

    working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,31 +1,29 @@
 class CocktailsController < ApplicationController
-  class CocktailsController < ApplicationController
-    def index
-      @cocktails = Cocktail.all
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
     end
+  end
 
-    def new
-      @cocktail = Cocktail.new
-    end
+  private
 
-    def show
-      @cocktail = Cocktail.find(params[:id])
-    end
-
-    def create
-      @cocktail = Cocktail.new(cocktail_params)
-
-      if @cocktail.save
-        redirect_to cocktails_path
-      else
-        render :new
-      end
-    end
-
-    private
-
-    def cocktail_params
-      params.require(:cocktail).permit(:name, :picture_url)
-    end
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :picture_url)
   end
 end
